77dd9b3ad5fe5f23423f65d13fb8c9924b330347
FortunateSon999 <qitheodore.lee@my.jcu.edu.au>
2023-09-16 22:03:25 +0800

    Fixed silver_service_taxi.py code, and finished silver_service_taxi_test.py.

--- Prac_09/silver_service_taxi.py
+++ Prac_09/silver_service_taxi.py
@@ -16,12 +16,11 @@ class SilverServiceTaxi(Taxi):
         self.fanciness = fanciness
         # Now each object instance will be created with current_fare_distance initialized at 0.
         self.current_fare_distance = 0
-        self.price_per_km = Taxi.price_per_km * fanciness
+        self.price_per_km *= fanciness
 
     def __str__(self):
-        return (f"{super().__str__()}, {self.current_fare_distance}km on current fair, {self.price_per_km}/km "
-                f"plus flagfall of {self.flagfall}")
+        return f"{super().__str__()}, plus flagfall of ${self.flagfall:.2f}"
 
     def get_fare(self):
         """Return the price of the trip."""
-        return self.flagfall + (self.price_per_km * self.fanciness)
+        return self.flagfall + (self.price_per_km * self.current_fare_distance)
