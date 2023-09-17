eea99985c5acbb7a2bfd8100eb7f9a28fd22cc94
FortunateSon999 <qitheodore.lee@my.jcu.edu.au>
2023-09-17 14:07:22 +0800

    Finished code for band.py.

--- Prac_09/band.py
+++ Prac_09/band.py
@@ -15,13 +15,15 @@ class Band:
 
     def __str__(self):
         """Return a string representation of the band."""
-        return f"{self.name} ({self.musicians[0]}{self.musicians[1]}{self.musicians[2]}{self.musicians[3]})"
+        musician_info = ", ".join(str(musician) for musician in self.musicians)
+        return f"{self.name} ({musician_info})"
 
     def add(self, musician):
         """Adds a musician to the band."""
         self.musicians.append(musician)
 
     def play(self):
+        result = ""
         for musician in self.musicians:
-            print(musician.play())
-2
+            result += f"{musician.play()}\n"
+        return result
--- Prac_09/my_band.py
+++ Prac_09/my_band.py
@@ -21,16 +21,5 @@ def main():
     print(band)
     print("band.play()")
     print(band.play())
-    """
-    Output: 
-    band (str)
-    Extreme (Nuno Bettencourt ([Washburn N4 (1990) : $2,499.95, Takamine acoustic (1986) : $1,200.00]), Gary Cherone ([]), Pat Badger ([Mouradian CS-74 Bass (2009) : $1,500.00]), Kevin Figueiredo ([]))
-    band.play()
-    Nuno Bettencourt is playing: Washburn N4 (1990) : $2,499.95
-    Gary Cherone needs an instrument!
-    Pat Badger is playing: Mouradian CS-74 Bass (2009) : $1,500.00
-    Kevin Figueiredo needs an instrument!
-    """
-
 
 main()
